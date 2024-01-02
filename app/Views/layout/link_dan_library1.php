912c34ba2a6ecca61da2ceea3e1e642ed0c53a7f
AgungRamadhani26 <agungramadhani@students.undip.ac.id>
2024-01-02 15:50:18 +0700

    Library_fe

--- app/Views/layout/link_dan_library1.php
+++ app/Views/layout/link_dan_library1.php
@@ -1,12 +1,12 @@
 <!-- Vendor JS Files -->
-<script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendor/chart.js/chart.umd.js"></script>
-<script src="assets/vendor/echarts/echarts.min.js"></script>
-<script src="assets/vendor/quill/quill.min.js"></script>
-<script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
-<script src="assets/vendor/tinymce/tinymce.min.js"></script>
-<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/library_fe/apexcharts/apexcharts.min.js"></script>
+<script src="assets/library_fe/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/library_fe/chart.js/chart.umd.js"></script>
+<script src="assets/library_fe/echarts/echarts.min.js"></script>
+<script src="assets/library_fe/quill/quill.min.js"></script>
+<script src="assets/library_fe/simple-datatables/simple-datatables.js"></script>
+<script src="assets/library_fe/tinymce/tinymce.min.js"></script>
+<script src="assets/library_fe/php-email-form/validate.js"></script>
 
 <!-- Template Main JS File -->
 <script src="assets/js/main.js"></script>
