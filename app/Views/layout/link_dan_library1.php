8e98a3658a34db9263c3f702d28f9937e1725f2b
AgungRamadhani26 <agungramadhani@students.undip.ac.id>
2024-06-04 13:35:06 +0700

    hapus library tidak dipakai, karena sudah memakai yang online

--- app/Views/layout/link_dan_library1.php
+++ app/Views/layout/link_dan_library1.php
@@ -1,12 +1,8 @@
 <!-- Vendor JS Files -->
-<script src="/assets/library_fe/apexcharts/apexcharts.min.js"></script>
 <script src="/assets/library_fe/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="/assets/library_fe/chart.js/chart.umd.js"></script>
-<script src="/assets/library_fe/echarts/echarts.min.js"></script>
 <script src="/assets/library_fe/quill/quill.min.js"></script>
 <script src="/assets/library_fe/simple-datatables/simple-datatables.js"></script>
 <script src="/assets/library_fe/tinymce/tinymce.min.js"></script>
-<script src="/assets/library_fe/php-email-form/validate.js"></script>
 <!-- Sweet Alert -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <!-- Template Main JS File -->
